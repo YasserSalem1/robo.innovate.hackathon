04a51d248e788d47404fef4a48017bac4f3083e2
julianskywalker <julian.m.koechlin@gmail.com>
2025-03-20 10:07:45 +0100

    jk: improve logging

--- tree_care/hydrate_and_fertilize.py
+++ tree_care/hydrate_and_fertilize.py
@@ -3,19 +3,19 @@ import time
 import threading
 from datetime import datetime
 import json
-
 import random
+from write_to_db import write_log_to_db
 
 # Function to control a pump
 def control_pump(pump, seconds, arduino):
     if pump == 'fertilize':
         pin = 2
-        print(f"Fertilizing for {seconds} seconds...")
+        write_log_to_db(f"🌱 Fertilizing for {seconds} seconds...")
     elif pump == 'hydrate':
         pin = 7
-        print(f"Hydrating for {seconds} seconds...")
+        write_log_to_db(f"💧 Hydrating for {seconds} seconds...")
     else:
-        print("Invalid pump type. Use 'fertilize' or 'hydrate'.")
+        write_log_to_db("Invalid pump type. Use 'fertilize' or 'hydrate'.")
         return
 
     # Turn pump ON
@@ -48,32 +48,31 @@ def get_hydration_and_fertilizer_seconds_values(avg_meas_dict):
     # These thresholds and calculations are arbitrary and should be adjusted based on real requirements
     # Hydration logic based on humidity (HUM)
     if avg_meas_dict['HUM'] < 30:
-        hydration_seconds = 5
+        hydration_seconds = 9
     elif avg_meas_dict['HUM'] < 50:
-        hydration_seconds = 4
+        hydration_seconds = 7
     elif avg_meas_dict['HUM'] < 70:
-        hydration_seconds = 3
+        hydration_seconds = 6
     else:
-        hydration_seconds = 2
+        hydration_seconds = 5
 
     # Fertilizer logic based on Nitrat (N), Kalium (K), and Phosphor (P)
     if avg_meas_dict['N'] < 10 or avg_meas_dict['K'] < 10 or avg_meas_dict['P'] < 10:
-        fertilizer_seconds = 5
+        fertilizer_seconds = 6
     elif avg_meas_dict['N'] < 20 or avg_meas_dict['K'] < 20 or avg_meas_dict['P'] < 20:
-        fertilizer_seconds = 4
+        fertilizer_seconds = 5
     elif avg_meas_dict['N'] < 30 or avg_meas_dict['K'] < 30 or avg_meas_dict['P'] < 30:
-        fertilizer_seconds = 3
+        fertilizer_seconds = 4
     else:
-        fertilizer_seconds = 2
+        fertilizer_seconds = 3
     # # TODO: Implement logic to calculate hydration and fertilizer seconds based on avg_meas_dict
     # hydration_seconds = random.uniform(0, 4)
     # fertilizer_seconds = random.uniform(0, 4)
     return hydration_seconds, fertilizer_seconds
 
-def hydrate_fertilize_tree(avg_meas_dict):
+def hydrate_fertilize_tree(avg_meas_dict, arduino_port='/dev/ttyUSB0'):
     hydration_seconds, fertilizer_seconds = get_hydration_and_fertilizer_seconds_values(avg_meas_dict)
-    #arduino = serial.Serial('/dev/ttyUSB0', 9600)
-    arduino = serial.Serial('/dev/tty.usbserial-B0015QAP', 9600)
+    arduino = serial.Serial(arduino_port, 9600)
     # Wait for the Arduino to initialize
     time.sleep(2)
     if hydration_seconds > 0 and fertilizer_seconds > 0:
@@ -107,8 +106,10 @@ if __name__ == '__main__':
     # Close the Arduino serial connection
     arduino.close()
 
+
+    #arduino = serial.Serial('/dev/tty.usbserial-B0015QAP', 9600)
     hydrate_fertilize_tree(
-        {
+        avg_meas_dict={
             'HUM': 31.85155849933511,
             'TEM': 10.742406770876368,
             'PH': 11.769778653356132,
@@ -116,5 +117,6 @@ if __name__ == '__main__':
             'N': 44.26549355396079,
             'K': 55.90387106814525,
             'P': 91.82894587693659
-        }
+        },
+        arduino_port='/dev/tty.usbserial-B0015QAP' # '/dev/ttyUSB0' on raspi
     )
--- tree_care/main.py
+++ tree_care/main.py
@@ -4,56 +4,71 @@ import random
 from hydrate_and_fertilize import hydrate_fertilize_tree
 # from write_to_db_demo import write_to_db, write_log_to_db
 from write_to_db import write_to_db, write_log_to_db
-from ir_camera import get_ndvi_value
+from ir_camera import get_ndvi_value, get_ndvi_value_demo
 from soil_and_hum_sensors import get_avg_measurement_outputs, SoilSensor, HumiditySensor, DemoHumiditySensor, DemoSoilSensor
+
+HUMIDITY_SENSOR_THRESHOLD = 7
 
 # main application file, runs the whole time
 demo_mode = False
 raspberry_pi = True
+camera_demo_mode = True
 if demo_mode:
     humidity_sensor = DemoHumiditySensor()
     soil_sensor = DemoSoilSensor()
 elif raspberry_pi:
-    humidity_sensor = HumiditySensor(arduino_port='/dev/ttyACM0')
-    soil_sensor = SoilSensor(arduino_port='/dev/ttyACM1')
+    humidity_sensor = HumiditySensor(arduino_port='/dev/ttyACM1')
+    soil_sensor = SoilSensor(arduino_port='/dev/ttyACM0')
+    pump_arduino_port = '/dev/ttyUSB0'
 else:
     humidity_sensor = HumiditySensor(arduino_port='/dev/tty.usbmodem141101')
     soil_sensor = SoilSensor(arduino_port='/dev/tty.usbmodem142101')
+    pump_arduino_port = '/dev/tty.usbserial-B0015QAP'
 
 def monitor_sensors():
     while True:
-        print(humidity_sensor.read())
-        print(soil_sensor.read())
-        if humidity_sensor.get_only_hum_value() > 7:
+        print(f"🔍 Sensors in soil? {humidity_sensor.get_only_hum_value() > HUMIDITY_SENSOR_THRESHOLD}")
+        # print(humidity_sensor.read())
+        # print(soil_sensor.read())
+        if humidity_sensor.get_only_hum_value() > HUMIDITY_SENSOR_THRESHOLD:
             t0 = time.time()
+            write_log_to_db(f"🚀 Yeahaa: Sensors in soil, starting measurement!")
             humidity_meas_dict = {}
             soil_meas_dict = {}
             for meas_key in humidity_sensor.measurement_keys:
                 humidity_meas_dict.update({meas_key: []})
             for meas_key in soil_sensor.measurement_keys:
                 soil_meas_dict.update({meas_key: []})
-            write_log_to_db("Humidity > 0, starting measurement.")
+            write_log_to_db(f"⏳ Measuring for about 5 seconds...")
             
             while time.time() - t0 < 6:
                 t_meas = time.time() - t0
                 hum_sensor_data_dict = humidity_sensor.read()
-                print(hum_sensor_data_dict)
+                #print(hum_sensor_data_dict)
                 soil_sensor_data_dict = soil_sensor.read()
-                print(soil_sensor_data_dict)
+                #print(soil_sensor_data_dict)
                 for key, value in hum_sensor_data_dict.items():
                     humidity_meas_dict[key].append(value)
                 for key, value in soil_sensor_data_dict.items():
                     soil_meas_dict[key].append(value)
                 time.sleep(1)  # Simulate sensor reading interval
             total_meas_time = time.time() - t0
-            write_log_to_db(f"Measurement completed after {total_meas_time} seconds.")
-            write_log_to_db(f"Analysing measured data, then fertilize and hydrate.")
+            write_log_to_db(f"✅ Measurement completed after {total_meas_time} seconds.")
+            write_log_to_db(f"🔍 Analysing data to hydrate and fertilize respectively...")
             avg_meas_dict = get_avg_measurement_outputs({**humidity_meas_dict, **soil_meas_dict})
-            hydration_fertilize_dict = hydrate_fertilize_tree(avg_meas_dict)
-            write_log_to_db(f"Hydration and fertilize completed, taking foto and calculating ndvi-value.")
-            foto_arr, ndvi_val = get_ndvi_value()
+            mean_val_str = ', '.join([f"{key}: {value}" for key, value in avg_meas_dict.items()])
+            write_log_to_db(f"📊 Measured mean values: {mean_val_str}")
+            hydration_fertilize_dict = hydrate_fertilize_tree(avg_meas_dict, arduino_port=pump_arduino_port)
+            write_log_to_db(f"✅ Hydration and fertilization completed.")
+            # taking foto and calculating ndvi-value.
+            write_log_to_db(f"🎥 Taking foto and calculating NDVI-value...")
+            if camera_demo_mode:
+                foto_arr, ndvi_val = get_ndvi_value_demo()
+            else:
+                foto_arr, ndvi_val = get_ndvi_value()
+            write_log_to_db(f"✅ NDVI-value: {ndvi_val}")
             write_to_db(total_meas_time, hydration_fertilize_dict, avg_meas_dict, foto_arr, ndvi_val, )
-            write_log_to_db("Overall measurement completed and data written to DB.")
+            write_log_to_db("🎉😃 Overall measurement completed and data written to DB.")
         time.sleep(1)  # Check humidity_sensor value every second
 
 if __name__ == "__main__":
